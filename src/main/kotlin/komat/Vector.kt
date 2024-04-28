532b900eb7274e9eeda0e52a90256c21921009cb
volta2030 <volta2030@gmail.com>
2024-04-28 22:37:34 +0900

    feat : vector dotProduct 추가

--- src/main/kotlin/komat/Vector.kt
+++ src/main/kotlin/komat/Vector.kt
@@ -1,5 +1,16 @@
 package komat
 
 class Vector(vararg _elements: Number) {
-    var elements = _elements.map(Number::toDouble).toMutableList();
+    var elements = _elements.map(Number::toDouble).toMutableList()
+
+    fun dotProduct(_vector: Vector): Double {
+
+        var result = 0.0
+
+        elements.forEachIndexed { indexed, it ->
+            result += it * _vector.elements[indexed]
+        }
+
+        return result
+    }
 }

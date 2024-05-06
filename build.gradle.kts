243ae72be79e25f6491ef42eb8838fbe03ccd497
volta2030 <volta2030@gmail.com>
2024-05-06 17:52:18 +0900

    feat : Version Up

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ publishing {
             groupId = "com.volta2030"
             artifactId = "komat"
 
-            version = "1.3.0"
+            version = "1.4.0"
 
             from(components["java"])
         }

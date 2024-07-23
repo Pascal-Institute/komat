1696386f6744056107a37e073daaf6add398464e
volta2030 <volta2030@gmail.com>
2024-07-23 22:30:37 +0900

    fix : define 6 default operator +, +=, -, -=, *, /

--- src/main/kotlin/komat/Element.kt
+++ src/main/kotlin/komat/Element.kt
@@ -3,24 +3,29 @@ package komat
 sealed class Element {
     data class DoubleElement(var value: Double) : Element() {
         override fun getValue(): Any = value
-        override fun div(value: Element): Element {
-            return Element(getValue() as Double / value.getValue() as Double)
-        }
 
-        override fun times(value: Element): Element {
-            return Element((getValue() as Double) * value.getValue() as Double)
+        override fun plus(value: Element) {
+            this.value + value.getValue() as Double
         }
 
         override fun plusAssign(value: Element) {
             this.value += value.getValue() as Double
         }
 
+        override fun minus(value: Element) {
+            this.value - value.getValue() as Double
+        }
+
         override fun minusAssign(value: Element) {
             this.value -= value.getValue() as Double
         }
 
-        override fun divAssign(value: Element) {
-            this.value /= value.getValue() as Double
+        override fun times(value: Element): Element {
+            return Element((getValue() as Double) * value.getValue() as Double)
+        }
+
+        override fun div(value: Element): Element {
+            return Element(getValue() as Double / value.getValue() as Double)
         }
 
         override fun unaryMinus(): Element {
@@ -30,24 +35,29 @@ sealed class Element {
 
     data class ByteElement(var value: Byte) : Element() {
         override fun getValue(): Any = value
-        override fun div(value: Element): Element {
-            return Element(getValue() as Byte / value.getValue() as Byte)
-        }
 
-        override fun times(value: Element): Element {
-          return Element((getValue() as Byte) * value.getValue() as Byte)
+        override fun plus(value: Element) {
+            this.value + value.getValue() as Byte
         }
 
         override fun plusAssign(value: Element) {
             this.value = (getValue() as Byte + value.getValue() as Byte).toByte()
         }
 
+        override fun minus(value: Element) {
+            this.value - value.getValue() as Byte
+        }
+
         override fun minusAssign(value: Element) {
             this.value = (getValue() as Byte - value.getValue() as Byte).toByte()
         }
 
-        override fun divAssign(value: Element) {
-            this.value = (getValue() as Byte / value.getValue() as Byte).toByte()
+        override fun times(value: Element): Element {
+            return Element((getValue() as Byte) * value.getValue() as Byte)
+        }
+
+        override fun div(value: Element): Element {
+            return Element(getValue() as Byte / value.getValue() as Byte)
         }
 
         override fun unaryMinus(): Element {
@@ -58,24 +68,29 @@ sealed class Element {
 
     data class BooleanElement(var value: Boolean) : Element(){
         override fun getValue(): Any = value
-        override fun div(value: Element): Element {
-            TODO("Not yet implemented")
-        }
 
-        override fun times(value: Element): Element {
-            return Element((getValue() as Boolean) && value.getValue() as Boolean)
+        override fun plus(value: Element) {
+            this.value || value.getValue() as Boolean
         }
 
         override fun plusAssign(value: Element) {
             this.value = (getValue() as Boolean ||  value.getValue() as Boolean)
         }
 
-        override fun minusAssign(value: Element) {
-            TODO("Not yet implemented")
+        override fun minus(value: Element) {
+           this.value || !(value.getValue() as Boolean)
         }
 
-        override fun divAssign(value: Element) {
-            TODO("Not yet implemented")
+        override fun minusAssign(value: Element) {
+            this.value = (getValue() as Boolean) || !(value.getValue() as Boolean)
+        }
+
+        override fun times(value: Element): Element {
+            return Element((getValue() as Boolean) && value.getValue() as Boolean)
+        }
+
+        override fun div(value: Element): Element {
+            return Element((getValue() as Boolean) && !(value.getValue() as Boolean))
         }
 
         override fun unaryMinus(): Element {
@@ -124,10 +139,11 @@ sealed class Element {
         }
     }
 
-    abstract operator fun div(value: Element): Element
-    abstract operator fun times(value: Element): Element
+    abstract operator fun plus(value: Element)
     abstract operator fun plusAssign(value: Element)
+    abstract operator fun minus(value: Element)
     abstract operator fun minusAssign(value: Element)
-    abstract operator fun divAssign(value: Element)
+    abstract operator fun times(value: Element): Element
+    abstract operator fun div(value: Element): Element
     abstract operator fun unaryMinus(): Element
 }
